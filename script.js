10d17a4133e714c783ffd614fa82c27ed4e0f7c8
aknur2306 <aknurkairat7@gmail.com>
2025-02-08 14:05:35 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+function generatePassword() {
+    const length = document.getElementById("length").value;
+    const includeUppercase = document.getElementById("uppercase").checked;
+    const includeLowercase = document.getElementById("lowercase").checked;
+    const includeNumbers = document.getElementById("numbers").checked;
+    const includeSymbols = document.getElementById("symbols").checked;
+
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*";
+
+    let allChars = "";
+    if (includeUppercase) allChars += uppercaseChars;
+    if (includeLowercase) allChars += lowercaseChars;
+    if (includeNumbers) allChars += numberChars;
+    if (includeSymbols) allChars += symbolChars;
+
+    if (allChars.length === 0) {
+        document.getElementById("password").innerText = "Выберите хотя бы один параметр!";
+        return;
+    }
+
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allChars.length);
+        password += allChars[randomIndex];
+    }
+
+    document.getElementById("password").innerText = password;
+}
